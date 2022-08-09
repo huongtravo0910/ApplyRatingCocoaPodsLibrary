f491fd354a7299cab5c4a803087064e4f3c72311
huongtravo0910 <huongtravo0910@gmail.com>
2022-08-09 12:17:24 +0700

    Initial Commit

---
+++ ApplyRatingCocoaPodsLibrary/ApplyRatingCocoaPodsLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplyRatingCocoaPodsLibraryApp.swift
+//  ApplyRatingCocoaPodsLibrary
+//
+//  Created by Tra Vo on 09/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ApplyRatingCocoaPodsLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
